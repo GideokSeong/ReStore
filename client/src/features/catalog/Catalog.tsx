b259d4d22b60cbf7f0af20a3bafe62a40c989129
Gideok Seong <gseong@my.bridgeport.edu>
2023-08-09 15:09:05 -0400

    Section 3

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,27 +1,21 @@
-import { Avatar, Button, List, ListItem, ListItemAvatar } from "@mui/material";
 import { Product } from "../../app/models/product";
+import ProductList from "./ProductList";
+import { useState, useEffect } from "react";
 
-interface Props {
-  products: Product[];
-  addProduct: () => void;
-}
 
-export default function Catalog({products, addProduct}: Props){
+export default function Catalog(){
+  const [products, setProducts] = useState<Product[]>([
+  ]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/products')
+      .then(response=>response.json())
+      .then(data => setProducts(data));
+  }, []);
+  
   return (
     <>
-      <List>
-      {products.map(product => (
-        <ListItem key={product.name}>
-          <ListItemAvatar>
-            <Avatar src={product.pictureUrl} />
-          </ListItemAvatar>
-          <ListItemAvatar>
-            {product.name} - {product.price}
-          </ListItemAvatar>
-        </ListItem>
-      ))}
-      </List>
-      <Button variant="contained" onClick={addProduct}>Add product</Button>
+      <ProductList products={products} />
     </>
   )
 }
