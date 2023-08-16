bf94888f7b9eeb7179b3b2272796f61c8182e5f0
Gideok Seong <gseong@my.bridgeport.edu>
2023-08-16 17:59:23 -0400

    Error handling

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import ProductDetails from "../../features/catalog/ProductDetails";
 import AboutPage from "../../features/about/AboutPage";
 import ContactPage from "../../features/contact/ContactPage";
@@ -6,6 +6,7 @@ import Catalog from "../../features/catalog/Catalog";
 import HomePage from "../../features/home/HomePage";
 import App from "../layout/App";
 import ServerError from "../errors/ServerError";
+import NotFound from "../errors/NotFound";
 
 export const router = createBrowserRouter([
     {
@@ -17,7 +18,8 @@ export const router = createBrowserRouter([
         {path: 'catalog/:id', element: <ProductDetails />},
         {path: 'about', element: <AboutPage />},
         {path: 'contact', element: <ContactPage />},
-        {path: 'server-error', element: <ServerError />},
+        {path: 'not-found', element: <NotFound />},
+        {path: '*', element: <Navigate replace to='/not-found' />}
       ]
     }
 ])
