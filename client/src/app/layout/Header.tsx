776a840b5146b26ceb7768b703bbfa12b59a3dba
Gideok Seong <gseong@my.bridgeport.edu>
2023-10-04 17:55:24 -0400

    Many updates for account login feature

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -2,6 +2,7 @@ import { ShoppingCart } from "@mui/icons-material";
 import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
 import { Link, NavLink } from "react-router-dom";
 import { useAppSelector } from "../store/configureStore";
+import SignedInMenu from "./SignedInMenu";
 
 const midLinks = [
   {title: 'catalog', path: '/catalog'},
@@ -36,6 +37,7 @@ interface Props {
 
 export default function Header({darkMode, handleThemeChange}: Props) {
   const {basket} = useAppSelector(state => state.basket);
+  const {user} = useAppSelector(state => state.account);
   const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0);
   
   return (
@@ -71,8 +73,11 @@ export default function Header({darkMode, handleThemeChange}: Props) {
                 <ShoppingCart />
               </Badge>
           </IconButton>
-
-          <List sx={{display: 'flex'}}>
+          
+          {user ? (
+            <SignedInMenu />
+          ): (
+            <List sx={{display: 'flex'}}>
             {rightLinks.map(({title, path}) =>(
                 <ListItem
                   component={NavLink}
@@ -83,7 +88,8 @@ export default function Header({darkMode, handleThemeChange}: Props) {
                   {title.toUpperCase()}
                 </ListItem>
             ))}
-          </List>
+            </List>
+          )}
         </Box>
       </Toolbar>
     </AppBar>
